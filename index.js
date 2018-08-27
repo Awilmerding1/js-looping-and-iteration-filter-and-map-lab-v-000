74b18bd263d1e12d8abd1cc4ca76ebf6572cca46
Annabel Wilmerding <annabelwilmerding@gmail.com>
2018-08-27 20:47:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solution here:
+
+function driversWithRevenueOver(drivers, revenue) {
+  const a = drivers.filter(function(driver) { return driver.revenue > revenue});
+	return a;
+}
+
+function driverNamesWithRevenueOver(drivers, revenue) {
+  const a = drivers.filter(function(driver) { return driver.revenue > revenue});
+  const names = a.map(function(b) { return b.name });
+	return names;
+}
+
+function exactMatch(drivers, {key: value}) {
+  const d = drivers.filter(function(driver) {return driver[key] === value})
+  return d;
+}
